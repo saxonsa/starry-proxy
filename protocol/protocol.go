0ac5bfec0f235a99672095bfb6975711fd755958
XingyunSA <saxonsa5683@163.com>
2021-11-09 16:43:08 +0800

    fix conflict

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -1,10 +1,8 @@
 package protocol
 
-const (
-	HTTPProxyProtocol = "/starry-proxy/http/0.0.1" // http or https stream
-	NewNodeEntryProtocol = "/starry-proxy/new-node-entry/0.0.1"
-	AssignSelfAsSupernodeProtocol = "/starry-proxy/assignSupernode/0.0.1"
-)
+const CommonProtocol = "/starry-proxy/http/0.0.1" // http or https stream
+
+const NewNodeEntryProtocol = "/starry-proxy/new-node-entry/0.0.1"
 
 const AssignSelfAsSupernode = "/starry-proxy/assign-self-as-supernode/0.0.1"
 
